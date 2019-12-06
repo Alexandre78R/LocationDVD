b62ec8ab0eb4f495e1b038ec13f8df5edda144a5
Alexandre78R <alexandre.renard98@gmail.com>
2019-12-06 12:05:19 +0100

    Ajout de redux avec les modules + le store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import './App.css';
+
 import DocumentTitle from 'react-document-title';
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Composants/Screen/Home';
 import NewsLocationDVD from './Composants/Screen/NewsLocationDVD';
 import NotFound from './Composants/Screen/NotFound';
+
+import {Provider} from 'react-redux';
+import {createStore, combineReducers}  from 'redux';
+import DVDS from './Composants/Reducer/ReducerDVDS';
+
+const store = createStore(combineReducers({DVDS}));
+
 
 class App extends React.Component {
 
   render() {
     return (
       <DocumentTitle title='Location de DVD'>
-        <Router>
-          <Switch>  
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/new-location-dvd" component={NewsLocationDVD}/>
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <Switch>  
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/new-location-dvd" component={NewsLocationDVD}/>
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </Router>
+        </Provider>
       </DocumentTitle>
     );
   }
