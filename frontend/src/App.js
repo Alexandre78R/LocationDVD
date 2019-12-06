74b275a31b263bdfc057730d23605c635230dda6
Alexandre78R <alexandre.renard98@gmail.com>
2019-12-06 11:11:30 +0100

    Ajout de react-document-title au front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import './App.css';
+import DocumentTitle from 'react-document-title';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Composants/Screen/Home';
 import NotFound from './Composants/Screen/NotFound';
@@ -10,12 +11,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <Router>
-        <Switch>  
-          <Route exact path="/" component={Home}/>
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </Router>
+      <DocumentTitle title='Location de DVD'>
+        <Router>
+          <Switch>  
+            <Route exact path="/" component={Home}/>
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Router>
+      </DocumentTitle>
     );
   }
 }
