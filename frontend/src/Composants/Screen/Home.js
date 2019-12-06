7261530bb1393859ed24e268aafb7468c25d87fd
Alexandre78R <alexandre.renard98@gmail.com>
2019-12-06 11:26:19 +0100

    Début optimisation du tableau des locations de dvd

--- frontend/src/Composants/Screen/Home.js
+++ frontend/src/Composants/Screen/Home.js
@@ -3,126 +3,80 @@ import {
   Container,
   Row,
   Table,
+  Alert,
 } from 'reactstrap';
 import NavBar from '../NavBar'
 
 class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          dvdData : [ 
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+            {name : "Nom", Adresse : "Adresse", nameFilm : "Nom du film", dateLocation : "jj/mm/yyyy", dateFin : "jj/mm/yyyy"},
+         ],
+        };
+    }
 
   render() { 
+    var dvdData = this.state.dvdData.map(
+        (message, i) => {
+          return (
+              <tr key={i}>
+                <td>{message.name}</td>
+                <td>{message.Adresse}</td>
+                <td>{message.nameFilm}</td>
+                <td>{message.dateLocation}</td>   
+                <td>{message.dateFin}</td>      
+              </tr>
+          )
+        }
+    );    
     return (
         <div>
           <NavBar/>
           <Container className="page">
             <Row>
             <Table>
-                <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Adresse</th>
-                    <th>Nom du film</th>
-                    <th>Date Location</th>
-                    <th>Date Fin</th>
-                </tr>
-                </thead>
-                <tbody>
+                {
+                  dvdData.length === 0 ? 
+                    <div>
+                      <Alert color="danger">
+                        <h1 className="alert-heading center-message-error-dvd">Aucune Location DVD est disponible pour l'instant !</h1>
+                      </Alert>
+                    </div>
+                  :
+                  <thead>
                     <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
+                      <th>Nom</th>
+                      <th>Adresse</th>
+                      <th>Nom du film</th>
+                      <th>Date Location</th>
+                      <th>Date Fin</th>
                     </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Adresse</td>
-                        <td>Nom du film</td>
-                        <td>Date Location</td>   
-                        <td>Date Fin</td>      
-                    </tr>
-                </tbody>
+                  </thead>
+                }
+                {
+                  dvdData.length === 0 ?
+                  null
+                  :
+                  <tbody>
+                  {dvdData}
+                  </tbody>
+                }
               </Table>
             </Row>
           </Container>
