715131f0c6e24489b0bba774bcccc80890ebdc8a
Alexandre78R <alexandre.renard98@gmail.com>
2019-12-06 10:55:26 +0100

    Ajout des routes sur le frontend pour la Home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './Composants/Screen/Home';
 
 class App extends React.Component {
 
   render() {
     return (
-      <h1>APP</h1>
+      <Router>
+        <Switch>  
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </Router>
     );
   }
 }
