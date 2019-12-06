7197202fd72021edb1927bf20181ef75b4a49def
Alexandre78R <alexandre.renard98@gmail.com>
2019-12-06 10:03:44 +0100

    Ajout du fichier à la connexion de la bdd et de cors dans l'app

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var cors = require('cors')
+
+require("./models/bdd")
+
 var app = express();
+
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
